6b6c9c18a0ebd191ad7637bc4e4a270466cd34ab
Parth Kikani <parthkikani02@gmail.com>
2024-06-08 11:39:14 +0300

    complete 01.js exercise and extra credit 1

---
+++ src/exercise/01-exercise.js
@@ -0,0 +1,40 @@
+// Simple Data-fetching
+// http://localhost:3000/isolated/exercise/01.js
+
+import * as React from "react";
+import { PokemonDataView, fetchPokemon } from "../pokemon";
+
+let pokemon;
+
+const pokemonPromise = fetchPokemon("pikachu");
+
+pokemonPromise.then((pokemonData) => (pokemon = pokemonData));
+
+function PokemonInfo() {
+  if (!pokemon) {
+    throw pokemonPromise;
+  }
+
+  return (
+    <div>
+      <div className="pokemon-info__img-wrapper">
+        <img src={pokemon.image} alt={pokemon.name} />
+      </div>
+      <PokemonDataView pokemon={pokemon} />
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="pokemon-info-app">
+      <div className="pokemon-info">
+        <React.Suspense fallback={<div>Loading...</div>}>
+          <PokemonInfo />
+        </React.Suspense>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/exercise/01-extra-1.js
@@ -0,0 +1,53 @@
+// Simple Data-fetching
+// http://localhost:3000/isolated/exercise/01.js
+
+import * as React from "react";
+import {
+  PokemonDataView,
+  fetchPokemon,
+  PokemonErrorBoundary,
+} from "../pokemon";
+
+let pokemon, pokemonError;
+
+const pokemonPromise = fetchPokemon("pikachu");
+
+pokemonPromise.then(
+  (pokemonData) => (pokemon = pokemonData),
+  (error) => (pokemonError = error)
+);
+
+function PokemonInfo() {
+  if (pokemonError) {
+    throw pokemonError;
+  }
+
+  if (!pokemon) {
+    throw pokemonPromise;
+  }
+
+  return (
+    <div>
+      <div className="pokemon-info__img-wrapper">
+        <img src={pokemon.image} alt={pokemon.name} />
+      </div>
+      <PokemonDataView pokemon={pokemon} />
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="pokemon-info-app">
+      <div className="pokemon-info">
+        <PokemonErrorBoundary>
+          <React.Suspense fallback={<div>Loading...</div>}>
+            <PokemonInfo />
+          </React.Suspense>
+        </PokemonErrorBoundary>
+      </div>
+    </div>
+  );
+}
+
+export default App;
