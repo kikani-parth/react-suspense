c8662c3a57044c6394aec0dc1d8c810020cb5b9a
Parth Kikani <parthkikani02@gmail.com>
2024-06-12 12:09:13 +0300

    complete 05.js exercise and extra credit 1, 2

--- src/exercise/05-extra-1.js
+++ src/exercise/05-extra-1.js
@@ -6,14 +6,11 @@ import {
   fetchPokemon,
   PokemonInfoFallback,
   PokemonForm,
+  PokemonDataView,
   PokemonErrorBoundary,
   getImageUrlForPokemon,
 } from "../pokemon";
 import { createResource } from "../utils";
-
-const PokemonInfo = React.lazy(() =>
-  import("../lazy/pokemon-info-render-as-you-fetch")
-);
 
 function preloadImage(src) {
   return new Promise((resolve) => {
@@ -21,6 +18,18 @@ function preloadImage(src) {
     img.src = src;
     img.onload = () => resolve(src);
   });
+}
+
+function PokemonInfo({ pokemonResource }) {
+  const pokemon = pokemonResource.data.read();
+  return (
+    <div>
+      <div className="pokemon-info__img-wrapper">
+        <img src={pokemonResource.image.read()} alt={pokemon.name} />
+      </div>
+      <PokemonDataView pokemon={pokemon} />
+    </div>
+  );
 }
 
 const SUSPENSE_CONFIG = {
